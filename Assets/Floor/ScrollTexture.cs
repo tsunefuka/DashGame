327317ed7dee20f1dacc383bd5d2d754bf8990ea
tsunefuka-k <tsunefuka-k@klab.com>
2014-12-01 18:22:16 +0900

    スクロールする床と走るUnityちゃんを追加

---
+++ Assets/Floor/ScrollTexture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour
+{
+	public float speed = 0.1f;
+
+	void Update ()
+	{
+		float y = Mathf.Repeat (Time.time * speed, 1);
+		Vector2 offset = new Vector2 (0, -1 * y);
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+}
---
+++ Assets/MyUnityChan/UnityChanController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class UnityChanController : MonoBehaviour
+{
+	private const float SPEED = 1.0f;
+
+	private Animator animator;
+	private int speedId;
+	
+	void Start()
+	{
+		this.animator = GetComponent<Animator> ();
+		this.speedId = Animator.StringToHash ("speed");
+				Debug.Log ("speed=" + this.speedId);
+	}
+
+	void Update()
+	{
+		this.Move ();
+	}
+
+	void Move()
+	{
+		float speed = 0.0f;
+		if (Input.GetKey(KeyCode.UpArrow)) 
+		{
+			speed = 1.0f * SPEED;
+		} 
+		else 
+		{
+			speed = 0.0f;
+		}
+		this.animator.SetFloat (this.speedId, speed);
+		this.transform.position += this.transform.forward * speed;
+
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			this.transform.Rotate (0, 10, 0);
+		} 
+		else if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			this.transform.Rotate (0, -10, 0);
+		}
+	}
+}
