47ef7936acc6727389569710c38500901639b047
tsunefuka-k <tsunefuka-k@klab.com>
2014-12-09 18:29:36 +0900

    プレイヤー操作クラスを整理

--- Assets/MyUnityChan/ThreeLineController.cs
+++ Assets/MyUnityChan/ThreeLineController.cs
@@ -6,15 +6,18 @@ public class ThreeLineController : MonoBehaviour
 {
 	private const float INTERVAL_INPUT = 10.0f;
 
+	public GameObject manager;
 	public int numLine = 3;
 	public int flipWidth = 1;
 
 	protected Animator animator;
+	protected Manager myManager;
 	protected int nowLine = 2;
 
 	void Start ()
 	{
 		this.animator = GetComponent<Animator> ();
+		this.myManager = this.manager.GetComponent<Manager> ();
 	}
 	
 	void Update ()
@@ -24,12 +27,28 @@ public class ThreeLineController : MonoBehaviour
 
 	private void Move()
 	{
-		if (Input.GetButtonDown("right") && this.nowLine > 1) 
+		if (Input.GetButtonDown("right")) 
+		{
+			this.MoveRight ();
+		} 
+		else if (Input.GetButtonDown("left")) 
+		{
+			this.MoveLeft ();
+		}
+	}
+
+	public void MoveRight ()
+	{
+		if (this.nowLine > 1)
 		{
 			this.nowLine--;
 			this.transform.position += Vector3.forward * (-1) * this.flipWidth;
-		} 
-		else if (Input.GetButtonDown("left") && this.nowLine < this.numLine) 
+		}
+	}
+
+	public void MoveLeft ()
+	{
+		if (this.nowLine < this.numLine)
 		{
 			this.nowLine++;
 			this.transform.position += Vector3.forward * this.flipWidth;
